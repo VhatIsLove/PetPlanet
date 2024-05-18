d7557fa38672b0d63119084e2f28e02ef086105b
Vladislav Konovalov <vladislav.konovalov1@yandex.ru>
2024-05-18 22:53:52 +0300

    active modal and category

--- src/app.js
+++ src/app.js
@@ -1,25 +1,17 @@
-const button = document.querySelectorAll(".shop__category-btn");
+const API_URL = "https://melon-truth-protocol.glitch.me"; // адрес сервера
 
-const activeBtn = (event) => {
-	const target = event.target;
+const button = document.querySelectorAll(".shop__category-btn"); // получаем кнопку
+const productList = document.querySelector(".shop__list"); // получаем список
+const basketBtn = document.querySelector(".shop__btn-card"); // получаем кнопку карзины
+const modal = document.querySelector(".modal-overlay"); // получаем модальное окно
+const cartList = document.querySelector(".modal__cart-list"); // получаем список МО
 
-	button.forEach((btn) => {
-		btn.classList.remove("shop__category-btn_active");
-	});
-
-	target.classList.add("shop__category-btn_active");
-};
-
-button.forEach((btn) => {
-	btn.addEventListener("click", activeBtn);
-});
+const modalCloseBtn = document.querySelector(".modal-overlay__close-btn"); // получаем кнопку закрыть МО
 
 // API Block
-
-const API_URL = "https://melon-truth-protocol.glitch.me"; // адрес сервера
-const productList = document.querySelector(".shop__list");
-
-const createProductCard = (product) => {
+// const createProductCard = (product) =>  такой записью мы получаем обект данных и в нужных местах обрвщалмсь бы к его ключам вытаскивая значения
+// а нижней записью мы сразу записываем ключи в скобки (обычные и фигурные)и где нужно получаем их значения
+const createProductCard = ({ photoUrl, name, price }) => {
 	// сама карточка
 	const productCard = document.createElement("li"); // создаем элемент списка
 	productCard.classList.add("shop__item"); // добавляем класс
@@ -27,15 +19,15 @@ const createProductCard = (product) => {
 							<article class="shop__product product">
 								<img
 									class="product__img"
-									src="${API_URL}${product.photoUrl}"
-									alt="${product.name}"
+									src="${API_URL}${photoUrl}"
+									alt="${name}"
 									width="388"
 									height="261"
 								/>
 
-								<h3 class="product__title">${product.name}</h3>
+								<h3 class="product__title">${name}</h3>
 
-								<p class="product__price">${product.price}&nbsp;₽</p>
+								<p class="product__price">${price}&nbsp;₽</p>
 
 								<button class="product__add-card">Заказать</button>
 							</article>
@@ -49,7 +41,7 @@ const renderProducts = (products) => {
 
 	products.forEach((products) => {
 		// перебираем новый список
-		const productCard = createProductCard(products); // создаем саму карточку
+		const productCard = createProductCard(products); // создаем саму карточку, передаем объект данных
 		productList.append(productCard); // вставляем картточку на страницу в старый список
 	});
 };
@@ -61,7 +53,7 @@ const fetchProductCategory = async (category) => {
 			`${API_URL}/api/products/category/${category}`
 		);
 		if (!response.ok) {
-			throw new Error(`Something went wrong: ${response.status}`);
+			throw new Error(response.status);
 		}
 
 		const products = await response.json(); // Получаем данные
@@ -72,5 +64,42 @@ const fetchProductCategory = async (category) => {
 		console.error(`Ошибка запроса товара: ${error}`);
 	}
 };
+// event это объект который мы можем полчать, вызывавть методы, деструктурировать
+// получаем event для того что бы у него получить target и работать с ним
+//свойство tdrget позволяет нам выявить элемент по которому кликнули
+// записыаем свойство target в переменную
+const changeCategory = (event) => {
+	const target = event.target;
+	const category = target.textContent;
 
-fetchProductCategory("Домики");
+	button.forEach((btn) => {
+		btn.classList.remove("shop__category-btn_active"); //изначально удаляем модифекатор активности
+	});
+
+	target.classList.add("shop__category-btn_active");
+	fetchProductCategory(category);
+};
+
+button.forEach((btn) => {
+	btn.addEventListener("click", changeCategory);
+
+	//Узнаем присутствует ли у данной категории модифекатор активности: _active
+	if (btn.classList.contains("shop__category-btn_active")) {
+		fetchProductCategory(btn.textContent);
+	}
+});
+
+//вешаем событие на кнопку корзины
+basketBtn.addEventListener("click", () => {
+	modal.style.display = "flex"; // у МО меняем свойство на с none на флекс, благодаря этому оно появляется
+});
+
+//Другой вариант получения свойства tdrget, которое  позволяет нам выявить элемент по которому кликнули
+modal.addEventListener("click", ({ target }) => {
+	// если target строго равен МО тогда мы закрываем МО или клик произошел на элемент с классом
+	if (target === modal || target.closest(".modal-overlay__close-btn")) {
+		modal.style.display = "none";
+	}
+});
+
+// 23min
