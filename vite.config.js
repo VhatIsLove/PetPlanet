97c7640678b0e61d03f1d6ad4fa1ba3d093601b9
Vladislav Konovalov <vladislav.konovalov1@yandex.ru>
2024-05-20 23:22:01 +0300

    feat: add new style modal window

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 				main: resolve(__dirname, "src/index.html"),
-				shop: resolve(__dirname, "src/shop.html"),
+				store: resolve(__dirname, "src/shop.html"),
 			},
 		},
 	},
